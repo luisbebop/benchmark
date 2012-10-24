661549c5cfc683483a0f4920450f76e06e2768c4
luisbebop <luisbebop@gmail.com>
2012-10-24 20:01:25 -0200

    Change benchmark.

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"sync"
-	"sync/atomic"
 )
 
 func panicOnErr(msg string, err error) {
@@ -20,58 +18,41 @@ func main() {
 	nthreads := flag.Int("n", -1, "how many threads will open connection concurrently")
 	msg := flag.String("data", "hello", "what will those threads write")
 	host := flag.String("host", "localhost:1234", "all threads connect to host")
+	nreqs := flag.Int("req",1, "how many req per thread")
 	flag.Parse()
+	
 	if *nthreads<=0 {
 		fmt.Println("Must supply nthreads")
 		flag.Usage()
 		return
 	}
-
-	var step1, step2, step3 sync.WaitGroup
-	step1.Add(*nthreads)
-	step2.Add(1)
-	step3.Add(*nthreads)
-
-	var conc int32 = 0
-	var maxConc int32 = 0
-	var total int32 = 0
+	
+	fmt.Println("starting benchmark")
 	data := []byte(*msg+"\n")
 	for i:=0; i<*nthreads; i++ {
 		go func() {
-			conn, err := net.Dial("tcp", *host)
-			if err!=nil {
-				fmt.Println("dial:", err)
-				step1.Done()
-				step2.Wait()
-				step3.Done()
-				return
-			}
-			c := atomic.AddInt32(&conc, 1)
-			atomic.AddInt32(&total, 1)
-			for c>maxConc && !atomic.CompareAndSwapInt32(&maxConc, maxConc, c) {
-			}
-			defer step3.Done()
-			defer atomic.AddInt32(&conc, -1)
-			defer conn.Close()
-			step1.Done()
-			step2.Wait()
-			if nr, err := conn.Write(data); err!=nil || nr!=len(data) {
-				log.Fatalln("Unable to write", err)
-			}
-			reply := make([]byte, len(data))
-			if nr, err := conn.Read(reply); err!=nil || nr!=len(data) {
-				log.Fatalln("Unable to read", err)
-			}
-			panicOnErr("copy", err)
-			if bytes.Compare(reply, data)!=0 {
-				log.Fatalln("This is not an echo server: "+string(reply))
+			for j:=0; j<*nreqs; j++ {
+				conn, err := net.Dial("tcp", *host)
+				if err!=nil {
+					fmt.Println("dial:", err)
+					return
+				}
+				if nr, err := conn.Write(data); err!=nil || nr!=len(data) {
+					log.Fatalln("Unable to write", err)
+				}
+				reply := make([]byte, len(data))
+				if nr, err := conn.Read(reply); err!=nil || nr!=len(data) {
+					log.Fatalln("Unable to read", err)
+				}
+				
+				panicOnErr("copy", err)
+				
+				if bytes.Compare(reply, data)!=0 {
+					log.Fatalln("This is not an echo server: "+string(reply))
+				}
 			}
 		}()
 	}
-	step1.Wait()
-	step2.Done()
-	step3.Wait()
-	fmt.Println("Max conc:", maxConc)
-	fmt.Println("total:", total)
-	fmt.Println("conc:", conc)
+	fmt.Println("benchmark finished")
+	select {}
 }
