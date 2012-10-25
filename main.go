a8094c48fbc796b2dfbed27f1ac066d2527cfc09
luisbebop <luisbebop@gmail.com>
2012-10-24 22:44:12 -0200

    Changed atomic counters.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net"
 	"time"
+	"sync/atomic"
 )
 
 func panicOnErr(msg string, err error) {
@@ -16,6 +17,8 @@ func panicOnErr(msg string, err error) {
 }
 
 func main() {
+	var counter uint64 = 0
+	done := make(chan bool, 1)
 	nthreads := flag.Int("n", -1, "how many threads will open connection concurrently")
 	msg := flag.String("data", "hello", "what will those threads write")
 	host := flag.String("host", "localhost:1234", "all threads connect to host")
@@ -33,29 +36,64 @@ func main() {
 	for i:=0; i<*nthreads; i++ {
 		go func() {
 			for j:=0; j<*nreqs; j++ {
+				//connect to the socket
 				conn, err := net.Dial("tcp", *host)
 				if err!=nil {
 					fmt.Println("dial:", err)
 					return
 				}
+				
+				//more one socket connected
+				atomic.AddUint64(&counter, 1)
+				
+				//write data to the socket
 				if nr, err := conn.Write(data); err!=nil || nr!=len(data) {
 					log.Fatalln("Unable to write", err)
 				}
+				
+				//receive from the socket
 				reply := make([]byte, len(data))
 				if nr, err := conn.Read(reply); err!=nil || nr!=len(data) {
 					log.Fatalln("Unable to read", err)
 				}
-				
+
+				//check error on receive
 				panicOnErr("copy", err)
 				
+				//compare response from the socket
 				if bytes.Compare(reply, data)!=0 {
 					log.Fatalln("This is not an echo server: "+string(reply))
 				}
 				
+				//sleep 1 second for each req to the server
 				time.Sleep(time.Second * 1)
 			}
 		}()
 	}
+	
+	go func() {
+		var diffTransactions uint64 = 0
+		totalTransactions := (uint64)((*nthreads) * (*nreqs))
+		for {
+			//get the transaction counter every 1 second
+			counterPartial := atomic.LoadUint64(&counter)
+			
+			//print to stdout
+		    fmt.Printf("Transaction %v/%v tps:%v\n", counterPartial, totalTransactions, counterPartial - diffTransactions)
+			
+			//check if all transactions were proccessed
+			if counterPartial == totalTransactions {
+				done<-true
+			}
+			
+			//store the last counter to know how much new transactions were proccessed in the last second
+			diffTransactions = counterPartial
+			
+			//sleep 1 second to refresh the stdout
+			time.Sleep(time.Second * 1)
+		}
+	}()
+	
+	<-done
 	fmt.Println("benchmark finished")
-	select {}
 }
